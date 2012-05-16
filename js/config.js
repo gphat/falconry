ab163b58c8d1ff32af1695da682912ab27a83e09
Jay Shirley <j@shirley.im>
2012-05-16 06:07:08 -0700

    Updating config, tests.

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ var YUI_config = {
   groups : {
     falconry : {
       combine  : false,
-      base     : '../js/',
-      root     : '../js/',
+      base     : 'js/',
+      root     : 'js/',
       filter   : 'raw',
       modules  : {
         'falconry-models' : {
