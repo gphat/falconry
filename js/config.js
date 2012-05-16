ede6f1a774be0e313a43bd86f6f214516f686a73
Jay Shirley <j@shirley.im>
2012-05-16 05:52:11 -0700

    Adapting into its own files and a config loader for it.

---
+++ js/config.js
@@ -0,0 +1,17 @@
+var YUI_config = {
+  gallery: "gallery-2012.03.23-18-00",
+  groups : {
+    falconry : {
+      combine  : false,
+      base     : '../js/',
+      root     : '../js/',
+      filter   : 'raw',
+      modules  : {
+        'falconry-models' : {
+          path : 'falconry-models.js'
+          requires : [ 'model', 'model-list', 'gallery-model-sync-rest' ]
+        }
+      }
+    }
+  }
+};
